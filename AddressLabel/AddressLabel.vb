1d378ce6a3c1a8b6e428140006089674d2ff7e05
Elliot Heiner <elliotheiner1997@gmail.com>
2021-10-08 20:52:05 -0600

    Add code to update display label based on user input.

--- AddressLabel/AddressLabel.vb
+++ AddressLabel/AddressLabel.vb
@@ -6,10 +6,18 @@
     End Sub
 
     Private Sub DisplayLabelButton_Click(sender As Object, e As EventArgs) Handles DisplayLabelButton.Click
-        Me.DisplayLabel.Text = FirstNameTextBox.Text
+        Me.DisplayLabel.Text = FirstNameTextBox.Text & " " & LastNameTextBox.Text & vbNewLine &
+        StreetAddressTextBox.Text & vbNewLine & CityTextBox.Text & ", " & StateTextBox.Text & vbNewLine &
+        ZipTextBox.Text
     End Sub
 
     Private Sub ClearButton_Click(sender As Object, e As EventArgs) Handles ClearButton.Click
         DisplayLabel.Text = ""
+        FirstNameTextBox.Text = ""
+        LastNameTextBox.Text = ""
+        StreetAddressTextBox.Text = ""
+        CityTextBox.Text = ""
+        StateTextBox.Text = ""
+        ZipTextBox.Text = ""
     End Sub
 End Class
